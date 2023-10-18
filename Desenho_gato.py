316da03954631dca49b932666454e94d7173006c
Barelli <109447295+VictorBarelli@users.noreply.github.com>
2023-10-18 11:36:35 -0300

    Update Desenho_gato.py

--- Desenho_gato.py
+++ Desenho_gato.py
@@ -4,7 +4,7 @@ import math
 window = turtle.Screen()
 window.bgcolor("yellow")
 
-cursor = turtle.Turtle() # Cursor
+cursor = turtle.Turtle() 
 cursor.shape("turtle")
 cursor.color("black")
 cursor.speed(3)
@@ -31,19 +31,15 @@ def positionAlongCircle(x, y, radius, angle):
   return [x + (radius*math.sin(radians)),
             y + (radius*math.cos(radians))]
 
-# Draw the head
-
 movePenY(cursor, -150)
 cursor.circle(150)
 
-# Draw the nose
 
 noseMouthOffset = -15
 
 movePenY(cursor, -20 + noseMouthOffset)
 cursor.circle(20)
 
-# Draw the mouth
 
 movePen(cursor, -100, -20 + noseMouthOffset)
 cursor.right(90)
@@ -51,31 +47,22 @@ cursor.circle(50, 180)
 cursor.left(180)
 cursor.circle(50, 180)
 
-# Draw the eyes
 
 eyeSpacingX = 30
 eyePosY = 40
 eyeRadius = 30
 
-# Right eye
 
 movePen(cursor, eyeSpacingX, eyePosY)
 cursor.right(180)
 cursor.circle(eyeRadius, -180)
 
-# Left eye
-
 movePen(cursor, -eyeSpacingX, eyePosY)
 cursor.circle(eyeRadius, 180)
-
-# Draw the tongue
 
 movePen(cursor, -20, -60 + noseMouthOffset)
 cursor.circle(20, 180)
 
-# Draw the ears
-
-# Right ear
 
 earBeginAngle = 25
 earSize = 85
@@ -89,7 +76,6 @@ cursor.setposition(positionB[0], positionB[1])
 positionC = positionAlongCircle(0, 0, 150, earBeginAngle + earWidth * 2)
 cursor.setposition(positionC[0], positionC[1])
 
-# Left ear
 
 positionA = positionAlongCircle(0, 0, 150, -earBeginAngle)
 movePen(cursor, positionA[0], positionA[1])
@@ -100,11 +86,9 @@ cursor.setposition(positionB[0], positionB[1])
 positionC = positionAlongCircle(0, 0, 150, -earBeginAngle + -earWidth * 2)
 cursor.setposition(positionC[0], positionC[1])
 
-# Whiskers
 
 whiskerLength = 180
 
-# Right whiskers
 
 movePen(cursor, 50, -15)
 cursor.setheading(0)
@@ -114,7 +98,6 @@ movePen(cursor, 50, 0)
 cursor.left(5)
 cursor.forward(whiskerLength)
 
-# Left whiskers
 
 movePen(cursor, -50, -15)
 cursor.setheading(180)
